25ee416b431e4441f5c8cf9eaa316f913515841e
jelmerp <jelmerpoelstra@gmail.com>
2020-06-27 17:25:52 -0400

    Initial commit

--- R/meta.R
+++ R/meta.R
@@ -1,6 +1,9 @@
 ## First steps:
 ##    1. New Project > New Package
 
+usethis::use_git()
+usethis::use_github() # you have to have a PAT setup
+
 library(devtools)
 library(roxygen2)
 library(devtools)
